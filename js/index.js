4bae904cfed23b8181f6c163aa9af536694676ba
zxy111z <1627381928@qq.com>
2018-01-03 08:58:37 +0800

    1

--- js/index.js
+++ js/index.js
@@ -70,4 +70,54 @@
         });
     })
 }
+// 楼层跳转
+{
+    let topbar = document.querySelector(".topbar");
+    let leftbar=document.querySelector(".aside-nei");
+    window.onscroll = function () {
+        let st = document.documentElement.scrollTop;
+        // console.log(st);
+        if (st >= 900) {
+            topbar.style.top = "0";
+        } else {
+            topbar.style.top = "-50px";
+        }
+        if(st>=2830){
+            leftbar.style.display="block";
+        }else{
+            leftbar.style.display="none";
+        }
+        lists.forEach(function (value,index) {
+            if(st>=value.offsetTop){
+                for(let i=0;i<lists.length;i++){
+                    floors[i].classList.remove("active");
+                }
+                floors[index].classList.add("active");
+            }
+        })
+    };
 
+
+    let lists = document.querySelectorAll(".lists");
+    // console.log(lists);
+    let floors = document.querySelectorAll(".aside-nei li");
+    // console.log(floors);
+    floors.forEach (function (ele, index) {
+        ele.onclick = function () {
+            let ot = lists[index].offsetTop;
+            // console.log(ot);
+            let now = document.documentElement.scrollTop;
+            let speed = (ot - now) * 30 / 300;
+            let time = 0;
+            let t = setInterval(function () {
+                now += speed;
+                time += 30;
+                if (time === 300) {
+                    clearInterval(t);
+                    now = ot;
+                }
+                document.documentElement.scrollTop = now;
+            }, 30)
+        }
+    })
+}
