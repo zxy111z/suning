4a709ca46ba85eaf6058a4b6ce2aa186f5cef20c
zxy111z <1627381928@qq.com>
2018-01-22 08:20:05 +0800

    1

--- js/index.js
+++ js/index.js
@@ -72,52 +72,88 @@
 }
 // 楼层跳转
 {
-    let topbar = document.querySelector(".topbar");
-    let leftbar=document.querySelector(".aside-nei");
-    window.onscroll = function () {
+    let flag=true;
+    let totop=document.querySelector(".aside-di-1");
+    totop.onclick=function(){
         let st = document.documentElement.scrollTop;
-        // console.log(st);
-        if (st >= 900) {
-            topbar.style.top = "0";
-        } else {
-            topbar.style.top = "-50px";
-        }
-        if(st>=2830){
-            leftbar.style.display="block";
-        }else{
-            leftbar.style.display="none";
-        }
-        lists.forEach(function (value,index) {
-            if(st>=value.offsetTop){
-                for(let i=0;i<lists.length;i++){
-                    floors[i].classList.remove("active");
-                }
-                floors[index].classList.add("active");
+        let speed=st*30/400;
+        let t=setInterval(function(){
+            st-=speed;
+            if(st<=0){
+                st=0;
+                clearInterval(t);
             }
-        })
-    };
+            document.documentElement.scrollTop=st;
+        },30)
+    }
+
+
+    let topbar = document.querySelector(".topbar");
+    let leftbar = document.querySelector(".aside-nei");
+    window.onscroll = function () {
+        if(flag){
+            let st = document.documentElement.scrollTop;
+            // console.log(st);
+            if (st >= 900) {
+                topbar.style.top = "0";
+            } else {
+                topbar.style.top = "-50px";
+            }
+            if (st >= 2740) {
+                leftbar.style.display = "block";
+            } else {
+                leftbar.style.display = "none";
+            }
+            lists.forEach(function (value, index) {
+                if (st >= value.offsetTop-110) {
+                    for (let i = 0; i < lists.length; i++) {
+                        floors[i].classList.remove("active");
+                    }
+                    floors[index].classList.add("active");
+                }
+            })
+        }
+    }
 
 
     let lists = document.querySelectorAll(".lists");
-    // console.log(lists);
     let floors = document.querySelectorAll(".aside-nei li");
-    // console.log(floors);
-    floors.forEach (function (ele, index) {
+    floors.forEach(function (ele, index) {
         ele.onclick = function () {
             let ot = lists[index].offsetTop;
-            // console.log(ot);
             let now = document.documentElement.scrollTop;
-            let speed = (ot - now) * 30 / 300;
+            let speed = (ot - now) * 30 / 400;
             let time = 0;
+            flag=false;
             let t = setInterval(function () {
                 now += speed;
                 time += 30;
                 if (time === 300) {
                     clearInterval(t);
                     now = ot;
+                    flag=true;
                 }
                 document.documentElement.scrollTop = now;
             }, 30)
+        }
+    })
+}
+
+// 侧导航
+{
+    let xuan=document.querySelectorAll(".luobo-neikuan-left>li");
+    let ka=document.querySelectorAll('.luobo-neikuan .item-box');
+    xuan.forEach(function(value,index){
+        value.onmousemove=function(){
+            for(let i=0;i<xuan.length;i++){
+                ka[i].classList.remove('xianshi');
+            }
+            ka[index].classList.add("xianshi");
+        }
+        value.onmouseout=function(){
+            for(let i=0;i<xuan.length;i++){
+                ka[i].classList.remove('xianshi');
+            }
         }
     })
 }
